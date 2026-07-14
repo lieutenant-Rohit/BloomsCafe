1ab9759f2feb5a115f2eb8c896b2184e7ae78dab
Rohit <rohitbishtt69@gmail.com>
2026-07-14 15:56:06 +0530

    Repository Layer added

---
+++ src/main/java/com/bloomscafe/repository/OrderItemRepository.java
@@ -0,0 +1,14 @@
+package com.bloomscafe.repository;
+
+import com.bloomscafe.entity.OrderItem;
+import org.hibernate.query.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+
+    List<OrderItem> findByOrderId(Long orderId);
+}
---
+++ src/main/java/com/bloomscafe/repository/OrderRepository.java
@@ -0,0 +1,14 @@
+package com.bloomscafe.repository;
+
+import com.bloomscafe.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+    List<Order> findByUserid(Long userId);
+
+    List<Order> findByStatus(String status);
+}
