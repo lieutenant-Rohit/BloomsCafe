e305f1e479266d92a1ac1f3defdc163af4601d5f
Rohit <rohitbishtt69@gmail.com>
2026-07-22 18:40:19 +0530

    Implemented Caching

--- src/main/java/com/bloomscafe/service/CategoryService.java
+++ src/main/java/com/bloomscafe/service/CategoryService.java
@@ -3,10 +3,13 @@ package com.bloomscafe.service;
 import com.bloomscafe.entity.Category;
 import com.bloomscafe.exception.ResourceNotFoundException;
 import com.bloomscafe.repository.CategoryRepository;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class CategoryService {
@@ -18,22 +21,30 @@ public class CategoryService {
     }
 
     //Fetch All Categories (Now with Pagination!)
+    @Transactional(readOnly = true)
+    @Cacheable(value = "categories", key = "'all_' + #page + '_' + #size")
     public Page<Category> getAllCategories(int page, int size){
         Pageable pageable = PageRequest.of(page, size);
         return categoryRepository.findAll(pageable);
     }
 
     //Create a New Category
+    @Transactional
+    @CacheEvict(value = "categories", allEntries = true)
     public Category createCategory(Category category){
         return categoryRepository.save(category);
     }
 
     //Find Category by ID
+    @Transactional(readOnly = true)
+    @Cacheable(value = "categories", key = "#id")
     public Category getCategoryById(Long id)
     {
         return categoryRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Category Not Found with ID: "+ id));
     }
 
+    @Transactional
+    @CacheEvict(value = "categories", allEntries = true)
     public Category updateCategory(Long id, Category categoryDetails){
         Category existingCategory = categoryRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Category Not Found with ID: "+ id));
@@ -43,6 +54,8 @@ public class CategoryService {
         return categoryRepository.save(existingCategory);
     }
 
+    @Transactional
+    @CacheEvict(value = "categories", allEntries = true)
     public void deleteCategory(Long id){
         Category existingCategory = categoryRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Category Not Found with ID: "+ id));
